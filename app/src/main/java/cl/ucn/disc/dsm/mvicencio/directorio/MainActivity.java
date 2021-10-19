423d25a01560ad388a25f8f7b6c083427173df50
Miguel Vicencio <miguel.vicenciog@gmail.com>
2021-10-19 08:28:47 -0300

    chore[project]: initialize the project

---
+++ app/src/main/java/cl/ucn/disc/dsm/mvicencio/directorio/MainActivity.java
@@ -0,0 +1,13 @@
+package cl.ucn.disc.dsm.mvicencio.directorio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
